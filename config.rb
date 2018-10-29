db9f9edbc9c99a7c4b9662c8d93c87db689b8433
Renny Fadoju <rennyvbirds@yahoo.com>
2018-10-29 14:51:29 +0000

    Remove Google Analytics code and gem

--- config.rb
+++ config.rb
@@ -1,14 +1,5 @@
 require 'govuk_tech_docs'
-require 'middleman-google-analytics'
 
 GovukTechDocs.configure(self)
 
 activate :directory_indexes
-
-activate :google_analytics do |ga|
-  ga.tracking_id = 'UA-127779891-1'
-
-  ga.allow_linker = true
-
-  ga.domain_name = ['docs.wifi.service.gov.uk', 'wifi.service.gov.uk', 'admin.wifi.service.gov.uk']
-end
