1b2ce8d3b716bec79f2b73ca7fd79e090da9d39d
Tom Taylor <tommotaylor@gmail.com>
2019-03-01 14:19:40 +0000

    Use partial

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,2 @@
 //= require govuk_tech_docs
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WX4MM64');
+//= require google_tag_manager
