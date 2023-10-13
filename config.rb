3e6a21ec053d641716684484693241a2312c989e
szd55gds <david.pye@digital.cabinet-office.gov.uk>
2023-10-13 13:44:58 +0100

    Migrate to github pages

--- config.rb
+++ config.rb
@@ -1,10 +1,13 @@
 require 'govuk_tech_docs'
 
+set :relative_links, true
+
 configure :build do
     Middleman::Extensions::MinifyJavascript.config.setting(:compressor).value = proc {
         require 'uglifier'
         Uglifier.new(harmony: true)
     }
+    activate :relative_assets
 end
 
 GovukTechDocs.configure(self)
